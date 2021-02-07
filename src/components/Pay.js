08d94879c8f70cdbcb2be01748fb27ecf5bd0550
Samuel Fanimokun <35940016+1Busayo@users.noreply.github.com>
2021-02-07 17:16:01 +0100

    Update Pay.js

--- src/components/Pay.js
+++ src/components/Pay.js
@@ -50,13 +50,15 @@ const styles = {
 
 const Pay = ({ classes }) => {
   //constants
-     var vars = {};
+   
+function getUrlVars() {
+    var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
 }
-	
+
 function getUrlParam(parameter, defaultvalue){
     var urlparameter = defaultvalue;
     if(window.location.href.indexOf(parameter) > -1){
